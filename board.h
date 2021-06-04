e2f8511ec3b100926a5858ea0fe78d689e25491e
OSoufian <74008427+OSoufian@users.noreply.github.com>
2021-06-04 20:16:51 +0200

    Ajout du fichier board.h
    Fichierd'en-tête contenant les structures des cellules et du tableau ainsi que la déclaration des fonctions contenues dans board.c

---
+++ board.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef _INC_BOARD 
+#define _INC_BOARD
+
+typedef struct {
+    int value;
+    int color;
+    int row;
+    int column;
+} Cell;
+
+typedef struct {
+    int rows;
+    int columns;
+    Cell *content;
+} Board;
+
+Board create_board();
+Cell create_cell(int value, int color, int row, int column);
+
+void show_board(Board board);
+void show_cell(Cell cell);
