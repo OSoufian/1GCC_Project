4c971140f0435cfdc66013d8b72c1516a9b228a7
marceljordan0 <75436070+marceljordan0@users.noreply.github.com>
2021-06-05 13:36:58 +0200

    tableau

--- main.c
+++ main.c
@@ -1,75 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Définition des fonctions
-void show_board(struct Cell* grid);
-void show_cell(struct Cell cell);
-struct Board create_board();
-struct Cell create_cell(int value, int color, int x, int y);
+//structure pour la grille
+struct Grid
+{
+    char table[50][50];
 
-
-
-//Création d'une structure pour les cellules
-struct Cell {
-    int value;
-    int color;
-    int x;
-    int y;
 };
 
-//Création d'une structure pour les plateaux de jeu
-struct Board {
-    int x;
-    int y;
-    struct Cell *content;
-};
-
+void grid();
+void display_board(struct Grid board);
 
 int main()
 {
-    struct Board board = create_board();
+    grid();
     return 0;
 }
 
-//Afficher la grille
-void show_board(struct Board board) {
+//fonction qui contient les differentes configurations
+void grid() {
+    struct Grid tab1 = {
 
-    for (int i = 0; i < board.x; i++) {
-        for (int j = 0; j < board.y; j++) {
-            show_cell(board.content[i+j*i]);
+            {{'1', '2', '3', '4', '5'},
+                    {'1', '2', '3', '4', '5'},
+                    {'1', '2', '3', '4', '5'},
+                    {'1', ' ', '3', '4', '5'}},
+
+
+    };
+    struct Grid tab2 = {
+
+            {{'1', '2', '3', '4', '5'},
+                    {'1', '2', '3', '4', '5','9','4'},
+                    {'1', '2', '3', '4', '5'},
+                    {'1', '3', '3', '4', '5'}},
+    };
+
+    struct Grid board[2]={tab1,tab2}; //structure qui contient les differentes configuration
+    display_board(board[1]);
+}
+
+//fonction qui affiche les differentes configurations
+void display_board(struct Grid board){
+    int i, j;
+    for(i = 0; i < 10; i++)
+    {
+        for(j = 0; j < 10; j++)
+        {
+            if(board.table[i][j] == 0)
+            {
+                board.table[i][j]= ' ';
+            }
+            printf("%c ", board.table[i][j]);
         }
         printf("\n");
     }
-
-}
-
-//Afficher une cellule
-void show_cell(struct Cell cell){
-    printf(cell.value);
-}
-
-//Créer la grille
-struct Board create_board() {
-
-    struct Board board;
-    board.x = 3;
-    board.y = 3;
-    board.content = malloc(board.x * board.y * sizeof(struct Cell));
-
-    board.content[0] = create_cell(1, 0, 0, 0);  board.content[1] = create_cell(1, 0, 0, 1); board.content[2] = create_cell(1, 0, 0, 2);
-    board.content[3] = create_cell(1, 0, 1, 0);  board.content[4] = create_cell(2, 0, 1, 1); board.content[5] = create_cell(2, 0, 1, 2);
-    board.content[6] = create_cell(0, 1, 2, 0);  board.content[7] = create_cell(3, 0, 2, 1); board.content[8] = create_cell(3, 0, 2, 2);
-
-    return board;
-}
-
-//Création des cellules
-struct Cell create_cell(int value, int color, int x, int y)
-{
-    struct Cell cell;
-    cell.value = value;
-    cell.color = color;
-    cell.x = x;
-    cell.y = y;
-    return cell;
-}
+};
