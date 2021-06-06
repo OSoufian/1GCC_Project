5a30a7960272226baa5eb0cd535dfd81ced94c46
OSoufian <74008427+OSoufian@users.noreply.github.com>
2021-06-06 20:32:41 +0200

    Update board.c
    Actuellement la grille s'affiche et les bonnes couleurs selon la couleur de la cellule en structure. Reste le problème de la couleur du foreground à régler.

--- board.c
+++ board.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 #include "board.h"
 
+void print_color(const char* s, int color);
 
-//Création des cellules
-Cell create_cell(int value, int color, int row, int column)
+//CrÃ©ation des cellules
+Cell create_cell(int value, int color, int index)
 {
-    Cell cell = { .value = value, .color = color , .column = column, .row = row };
+    Cell cell = { .value = value, .color = color , .index = index };
     return cell;
 }
 
-//Créer la grille
+//CrÃ©er la grille
 Board create_board()
 {
 
-    Board board = { .columns = 3, .rows = 3, .content = malloc(board.columns * board.rows * sizeof(Cell)) };
+    Board board = { .columns = 3, .rows = 3, .content = malloc(board.columns * board.rows * sizeof(Cell))};
 
-    board.content[0] = create_cell(1, 0, 0, 0);  board.content[1] = create_cell(1, 0, 0, 1); board.content[2] = create_cell(1, 0, 0, 2);
-    board.content[3] = create_cell(1, 0, 1, 0);  board.content[4] = create_cell(2, 0, 1, 1); board.content[5] = create_cell(2, 0, 1, 2);
-    board.content[6] = create_cell(0, 1, 2, 0);  board.content[7] = create_cell(3, 0, 2, 1); board.content[8] = create_cell(3, 0, 2, 2);
+    board.content[0][0] = create_cell(1, 7, 0);  board.content[0][1] = create_cell(1, 7, 0); board.content[0][2] = create_cell(1, 7, 0);
+    board.content[1][0] = create_cell(1, 7, 0);  board.content[1][1] = create_cell(2, 7, 0); board.content[1][2] = create_cell(2, 7, 0);
+    board.content[2][0] = create_cell(0, 1, 0);  board.content[2][1] = create_cell(3, 7, 0); board.content[2][2] = create_cell(3, 7, 0);
 
     return board;
 }
 
 //Afficher une cellule
 void show_cell(Cell cell) {
-    printf("%i", cell.value);
+
+    char c[] = "X";
+    if (cell.value != 0) _itoa(cell.value, c, 10);
+    print_color(c, cell.color);
 }
 
 //Afficher la grille
@@ -34,9 +39,17 @@ void show_board(Board board) {
 
     for (int row = 0; row < board.rows; row++) {
         for (int column = 0; column < board.columns; column++) {
-            show_cell(board.content[row * board.rows + column]);
+            show_cell(board.content[row][column]);
         }
         printf("\n");
     }
 
+}
+
+void print_color(const char* s, int color)
+{
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(console, FOREGROUND_INTENSITY | color);
+    printf(s);
+    SetConsoleTextAttribute(console, FOREGROUND_INTENSITY | 7);
 }
