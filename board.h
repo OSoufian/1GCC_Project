287999b657d291c1924f2913de1d16d84cfb3bd7
OSoufian <74008427+OSoufian@users.noreply.github.com>
2021-06-07 20:31:31 +0200

    Update board.h
    Ajout dans Cell de Row et Column pour pouvoir faire des changements directement aux cellules grâce aux positions. Et aussi nuance entre active qui est la cellule en cours et selected qui sont les cellules séléctionnés lorsque l'on emprunte un chemin

--- board.h
+++ board.h
@@ -1,11 +1,16 @@
 #pragma once
 #ifndef _INC_BOARD 
 #define _INC_BOARD
+#include <stdbool.h>
 
 typedef struct {
     int value;
     int color;
     int index;
+    int row;
+    int column;
+    bool active;
+    bool selected;
 } Cell;
 
 typedef struct {
@@ -19,5 +24,10 @@ Board create_board();
 
 void show_cell(Cell cell);
 void show_board(Board board);
+void print_color(const char* s, int color);
+void print_selected_cell(const char* s, int front_color, int back_color);
+void select_chain(Board board);
+int get_next_color(int color);
+Cell get_active_cell(Board board);
 
 #endif
