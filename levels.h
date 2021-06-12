ffb639904552f1e88ea752bf4870baed189ee04c
OSoufian <74008427+OSoufian@users.noreply.github.com>
2021-06-12 21:41:44 +0200

    Update levels.h
    Ajout des 10 premiers niveaux.

--- levels.h
+++ levels.h
@@ -3,5 +3,35 @@
 #define _INC_LEVEL
 
 Board create_board_level_01();
+Board create_board_level_02();
+Board create_board_level_03();
+Board create_board_level_04();
+Board create_board_level_05();
+Board create_board_level_06();
+Board create_board_level_07();
+Board create_board_level_08();
+Board create_board_level_09();
+Board create_board_level_10();
+//Board create_board_level_11();
+//Board create_board_level_12();
+//Board create_board_level_13();
+//Board create_board_level_14();
+//Board create_board_level_15();
+//Board create_board_level_16();
+//Board create_board_level_17();
+//Board create_board_level_18();
+//Board create_board_level_19();
+//Board create_board_level_20();
+//Board create_board_level_21();
+//Board create_board_level_22();
+//Board create_board_level_23();
+//Board create_board_level_24();
+//Board create_board_level_25();
+//Board create_board_level_26();
+//Board create_board_level_27();
+//Board create_board_level_28();
+//Board create_board_level_29();
+//Board create_board_level_30();
+
 
 #endif
